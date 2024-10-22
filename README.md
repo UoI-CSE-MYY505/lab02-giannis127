67138f7d02cb3d5276afc8fc6960e037a8071ec6
giannis127 <giannis.engel127@gmail.com>
2024-10-22 23:27:03 +0300

    lab02.s

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## UoI-CSE-MYY505 lab02
 
 ## First-Name Last-Name
+## IOANNIS ANGELOPOULOS
 
 **TODO - Update your name in this readme. Leave 1 space after the ## to preserve the text formating (markdown)**
 
--- lab02.s
+++ lab02.s
@@ -11,6 +11,18 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+    li s0, 0            
+    li t0, 0           
+loop:
+    beq t0, a1, done  
+    lw t1, 0(a0)      
+    bne t1, a2, skip   
+    mv s0, a0         
+
+skip:
+    addi a0, a0, 4     
+    addi t0, t0, 1      
+    j loop             
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
